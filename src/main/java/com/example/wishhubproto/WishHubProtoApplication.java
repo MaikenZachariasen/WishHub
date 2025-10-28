677e921d255c0923efd15bee214c90875735651e
404BrainNotFound1000 <emilgurres@gmail.com>
2025-10-28 16:29:28 +0100

    Midlertidig projekt

---
+++ src/main/java/com/example/wishhubproto/WishHubProtoApplication.java
@@ -0,0 +1,13 @@
+package com.example.wishhubproto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishHubProtoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WishHubProtoApplication.class, args);
+	}
+
+}
