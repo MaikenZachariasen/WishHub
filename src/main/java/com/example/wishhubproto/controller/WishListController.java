fda4b95415c3fc47fca60f03bfd02efe751048fe
404BrainNotFound1000 <emilgurres@gmail.com>
2025-10-28 16:58:41 +0100

    Midlertidig projekt

--- src/main/java/com/example/wishhubproto/controller/WishListController.java
+++ src/main/java/com/example/wishhubproto/controller/WishListController.java
@@ -1,7 +1,9 @@
 package com.example.wishhubproto.controller;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
 
 @Controller
 public class WishListController {
---
+++ src/main/java/com/example/wishhubproto/model/Wish.java
@@ -0,0 +1,8 @@
+package com.example.wishhubproto.model;
+
+public class Wish {
+
+
+
+
+}
